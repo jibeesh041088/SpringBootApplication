d6043fb0d9ccca56b1be30f762f131f5019f867d
Jibeesh P V <jibeeshpv09@gmail.com>
2019-11-21 16:09:27 +0900

    Change main class name

--- src/main/java/com/springApplication/SpringBootTestApplication.java
+++ src/main/java/com/springApplication/SpringBootTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
-public class SpringHelloApplication {
+public class SpringBootTestApplication {
     public static void main(String[] args){
-        SpringApplication.run(SpringHelloApplication.class, args);
+        SpringApplication.run(SpringBootTestApplication.class, args);
     }
 }
